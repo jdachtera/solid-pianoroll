6642b4f17ceee7b3696e6299be749e8ba61cefe3
Jascha Dachtera <jascha.dachtera@gmail.com>
2022-12-22 12:09:11 +0100

    fix: grid flicker

--- src/PianoRollGrid.tsx
+++ src/PianoRollGrid.tsx
@@ -24,7 +24,7 @@ const PianoRollGrid = () => {
 
   const gridArray = createMemo(() => {
     const numberOfLines = Math.ceil(
-      context.horizontalViewPort.getVisibleRange() / gridDivisorTicks(),
+      context.horizontalViewPort.getVisibleRange() / gridDivisorTicks() + 1,
     );
     const startIndex = Math.floor(context.position / gridDivisorTicks());
 
@@ -87,36 +87,38 @@ const PianoRollGrid = () => {
             );
           }}
         </Index>
-        <Index each={keys}>
-          {(key) => {
-            const virtualDimensions = createMemo(() =>
-              context.verticalViewPort.getVirtualDimensions(127 - key().number, 1),
-            );
+        <Show when={!context.condensed}>
+          <Index each={keys}>
+            {(key) => {
+              const virtualDimensions = createMemo(() =>
+                context.verticalViewPort.getVirtualDimensions(127 - key().number, 1),
+              );
 
-            return (
-              <Show when={virtualDimensions().size > 0}>
-                <div
-                  class="PianoRoll-Grid-Key"
-                  style={{
-                    position: "absolute",
-                    top: `${virtualDimensions().offset}px`,
-                    height: `${virtualDimensions().size}px`,
-                    width: "100%",
-                    "background-color": key().isBlack ? "rgba(0,0,0,0.2)" : "none",
-                    "border-style": "solid",
-                    "border-color": "gray",
-                    "border-width": `${
-                      !key().isBlack && !blackKeys.includes((key().number + 1) % 12) ? "0.1px" : 0
-                    } 1px ${
-                      !key().isBlack && !blackKeys.includes((key().number - 1) % 12) ? "0.1px" : 0
-                    } 0`,
-                    "z-index": 1,
-                  }}
-                ></div>
-              </Show>
-            );
-          }}
-        </Index>
+              return (
+                <Show when={virtualDimensions().size > 0}>
+                  <div
+                    class="PianoRoll-Grid-Key"
+                    style={{
+                      position: "absolute",
+                      top: `${virtualDimensions().offset}px`,
+                      height: `${virtualDimensions().size}px`,
+                      width: "100%",
+                      "background-color": key().isBlack ? "rgba(0,0,0,0.2)" : "none",
+                      "border-style": "solid",
+                      "border-color": "gray",
+                      "border-width": `${
+                        !key().isBlack && !blackKeys.includes((key().number + 1) % 12) ? "0.1px" : 0
+                      } 1px ${
+                        !key().isBlack && !blackKeys.includes((key().number - 1) % 12) ? "0.1px" : 0
+                      } 0`,
+                      "z-index": 1,
+                    }}
+                  ></div>
+                </Show>
+              );
+            }}
+          </Index>
+        </Show>
       </div>
     </div>
   );
